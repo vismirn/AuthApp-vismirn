2a84a44c1a6bdc8ba4c3eedf16def08171b68ead
Viktor <smirn04@yandex.ru>
2020-05-10 12:04:54 +0300

    4 урок окончен

---
+++ AuthApp/SearchResponse.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResponse.swift
+//  AuthApp
+//
+//  Created by Viktor Smirnov on 10.05.2020.
+//  Copyright © 2020 Виктор Смирнов. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+    
+    
+
+}
+
+struct Track: Decodable {
+    var trackName: String
+    var artistName: String
+    
+}
--- AuthApp/SearchViewController.swift
+++ AuthApp/SearchViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class SearchViewController: UIViewController {
 
     let searchController = UISearchController(searchResultsController: nil)
-
+    let tableView = UITableView(frame: .zero, style: .plain)
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        
         setupElements()
         setupConstraints()
         
@@ -33,6 +34,14 @@ extension SearchViewController {
         view.backgroundColor = .cyan
         navigationItem.searchController = searchController
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MYcell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        searchController.searchBar.delegate = self
+        
+        
+        
         
     }
 }
@@ -42,6 +51,60 @@ extension SearchViewController {
 extension SearchViewController {
     func setupConstraints() {
     
+        view.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        
     }
 }
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MYcell", for: indexPath)
+        cell.textLabel?.text = "indexPath \(indexPath)"
+        return cell
+    }
+    
+    
+    
+    }
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //Тут мы будем делать сетевой запрос по URL строке
+//        print(searchText)
+        
+        let urlString = "https://itunes.apple.com/search?term=\(searchText)&limit=10"
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { (_) in
+            guard let url = URL(string: urlString) else { return }
+            URLSession.shared.dataTask(with: url) { (data, respons, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+//                print(data)
+                guard let data = data else { return }
+                do {
+                    let tracks = try JSONDecoder().decode(SearchResponse.self, from: data)
+                    tracks.results.forEach { (track) in
+                        print(track.trackName)
+                    }
+                } catch let jsonError {
+                    print(jsonError.localizedDescription)
+                }
+            }.resume()
+        })
+        
+        
+        
+        
+    }
+}
